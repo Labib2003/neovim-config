2f7f7179776a41bb351cad4c7834fc14ff743178
Labib2003 <labib.salimi30@gmail.com>
2024-05-29 15:35:53 +0600

    switched from copilot to codeium

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -72,9 +72,24 @@ return {
   },
 
   {
-    "github/copilot.vim",
-    event = "BufEnter",
-    config = function() vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { expr = true, silent = true }) end,
+    "Exafunction/codeium.vim",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-a>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+      vim.keymap.set(
+        "i",
+        "<c-;>",
+        function() return vim.fn["codeium#CycleCompletions"](1) end,
+        { expr = true, silent = true }
+      )
+      vim.keymap.set(
+        "i",
+        "<c-,>",
+        function() return vim.fn["codeium#CycleCompletions"](-1) end,
+        { expr = true, silent = true }
+      )
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+    end,
   },
 
   {
